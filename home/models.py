b9fa18933c1059c0285102f068bc9bdeaa149c2b
kingkunt4 <otegarichard@proton.me>
2025-09-05 20:23:29 +0100

    fixed tzdata issue and created database object for uploading new projects

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class newProject(models.Model):
+  name = models.CharField(max_length = 255)
+  description = models.TextField()
+  image = models.ImageField(upload_to= 'project_images')
+  
+  def __str__(self):
+    return self.name
